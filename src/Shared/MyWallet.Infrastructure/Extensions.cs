d43e5532b7743f9408ce833c620891e6c87e1aba
matjanko <mateusz.jankowski@prokonto.pl>
2023-03-11 14:56:28 +0100

    Change Swagger config

--- src/Shared/MyWallet.Infrastructure/Extensions.cs
+++ src/Shared/MyWallet.Infrastructure/Extensions.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using MyWallet.Infrastructure.ApiDoc;
+using Microsoft.OpenApi.Models;
 
 [assembly: InternalsVisibleTo("MyWallet.Api")]
 namespace MyWallet.Infrastructure;
@@ -15,7 +15,18 @@ internal static class Extensions
     public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddControllers();
-        services.AddApiDoc(configuration);
+        
+        services.AddSwaggerGen(options =>
+        {
+            options.SwaggerDoc("v1", new OpenApiInfo
+            {
+                Title = "MyWallet Api", 
+                Version = "v1"
+            });
+            
+            const string xmlFilename = "MyWallet.Debts.Api.xml";
+            options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+        });
         
         services.Configure<RouteOptions>(options =>
         {
@@ -30,7 +41,12 @@ internal static class Extensions
     {
         if (environment.IsDevelopment())
         {
-            app.UseApiDoc(configuration);
+            app.UseSwagger();
+            app.UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint("/swagger/my-wallet/swagger.json", "MyWallet Api");
+                options.RoutePrefix = "api/";
+            });
         }
 
         app.UseRouting();
